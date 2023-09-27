ed9fd0f626a0c516be8cc3d0e796de5deda41d7d
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-09-27 02:38:18 +0000

    Update body styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export const Layout = (props: { title: string; children?: any; }) => {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
         />
       </head>
-      <body style="padding: 1em 2em; background: linear-gradient(83.21deg,#3245ff 0%,#bc52ee 100%); color: white;">
+      <body style="padding: 1em 2em; background: linear-gradient(83.21deg,#3245ff 0%,#bc52ee 100%); color: white; height: 100vh;">
         <header>
           <h1>
             <a href="/" class="fa-inverse">Welcome to my portfolio</a>
